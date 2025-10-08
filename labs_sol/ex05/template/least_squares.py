a0a88e8c60e4085f4d8979955231e2d97efcfafa
tosigame <tosigame@gmail.com>
2025-10-08 18:25:45 +0200

    week 5

---
+++ labs_sol/ex05/template/least_squares.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""Exercise 3.
+
+Least Square
+"""
+
+import numpy as np
+
+
+def compute_loss(y, tx, w):
+    """Calculate the loss using either MSE or MAE.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        w: shape=(2,). The vector of model parameters.
+
+    Returns:
+        the value of the loss (a scalar), corresponding to the input parameters w.
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: compute loss by MSE
+    # ***************************************************
+    e = y - tx @ w # N - (N,2 x 2,1)
+    return float((e.T @ e) / (2*(len(y))))
+
+def least_squares(y, tx):
+    """Calculate the least squares solution.
+       returns mse, and optimal weights.
+
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+        mse: scalar.
+
+    >>> least_squares(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]))
+    (array([ 0.21212121, -0.12121212]), 8.666684749742561e-33)
+    """
+    # ***************************************************
+    # COPY YOUR CODE FROM EX03 HERE
+    # least squares: TODO
+    # returns optimal weights, MSE
+    # ***************************************************
+    A = tx.T @ tx
+    b = tx.T @ y
+    w = np.linalg.solve(A,b)
+    return w,compute_loss(y,tx,w)
